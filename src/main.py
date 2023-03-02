456a2bd6bc278f12000f9e3f284ce1030fb47a88
i-sergh <seripp28@gmail.com>
2023-03-02 16:33:33 +0300

    base template

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from search.router import router as search_router
+from pages.router import router as pages_router
 
 app = FastAPI(
     title = 'DocSearch'
     )
 
+app.mount('/static', StaticFiles(directory='pages/static'), name='static')
 app.include_router(search_router)
+app.include_router(pages_router)
 
 @app.get('/')
 def hello():
